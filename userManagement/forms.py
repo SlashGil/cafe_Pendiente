8c411c1f05baa0d72c3ca49134d24c76c25dde14
Salvador Romero Gil <salvador.romero@justo.mx>
2023-09-22 23:32:07 -0600

    Set max_length

--- userManagement/forms.py
+++ userManagement/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 
+from coffee.variables.ModelsConstants import MAX_LENGTH_USER_NAME
 from userManagement.models import User
 
 
@@ -9,6 +10,6 @@ class SignIn(ModelForm):
         model = User
         fields = ['email', 'name', 'username', 'password']
         widgets = {
-            'name': forms.CharField(),
+            'name': forms.CharField(max_length=MAX_LENGTH_USER_NAME),
             'password': forms.PasswordInput()
         }
