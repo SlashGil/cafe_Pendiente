7e2dacac5a405c2de92cec3ec2dc006d4a91b10e
Salvador Romero Gil <salvador.romero@justo.mx>
2023-09-22 23:21:04 -0600

    Change class by def

--- userManagement/views.py
+++ userManagement/views.py
@@ -1,20 +1,24 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView
 
+from userManagement.forms import SignIn
 from userManagement.models import User
 
 
 # Create your views here.
-class CreateUser(LoginRequiredMixin, CreateView):
-    model = User
-    fields = ['email', 'first_name', 'last_name', 'password']
-    template_name = 'registration/signin.html'
-    success_url = reverse_lazy('home')
+def CreateUser(request):
+    if request.method == 'POST':
+        user = request.POST
+        userToSave = User(email=user.get('email'),
+                          name=user.get('name'),
+                          username=user.get('username'),
+                          password=user.get('password'))
+        userToSave.save()
+        return redirect('/')
+    else:
+        context = {}
+        context['form'] = SignIn()
+        return render(request,'registration/signin.html',context)
 
-
-class DeleteUser(LoginRequiredMixin, DeleteView):
-    model = User
-    template_name = 'delete_user.html'
-    success_url = reverse_lazy('home')
