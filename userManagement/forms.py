6213de7077afbf8f29b0b7517ad9df027830fa0e
Salvador Romero Gil <salvador.romero@justo.mx>
2023-09-22 23:36:54 -0600

    Change forms of user

--- userManagement/forms.py
+++ userManagement/forms.py
@@ -1,15 +1,13 @@
 from django import forms
 from django.forms import ModelForm
 
-from coffee.variables.ModelsConstants import MAX_LENGTH_USER_NAME
+from coffee.variables.ModelsConstants import MAX_LENGTH_USER_NAME, MAX_LENGTH_PASSWORD
 from userManagement.models import User
 
 
-class SignIn(ModelForm):
-    class Meta:
-        model = User
-        fields = ['email', 'name', 'username', 'password']
-        widgets = {
-            'name': forms.CharField(max_length=MAX_LENGTH_USER_NAME),
-            'password': forms.PasswordInput()
-        }
+class SignIn(forms.Form):
+    email = forms.EmailField()
+    name = forms.CharField(max_length=MAX_LENGTH_USER_NAME)
+    username = forms.CharField(max_length=MAX_LENGTH_USER_NAME)
+    password = forms.CharField(max_length=MAX_LENGTH_PASSWORD, widget=forms.PasswordInput)
+
